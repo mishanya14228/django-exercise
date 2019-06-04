100a7328b906d1215d6e3975a3dd847507864a56
Oysiyl <logart1995@gmail.com>
2019-06-04 17:21:55 +0300

    Added Models

--- main/api/admin.py
+++ main/api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Organization, Department, Employee
 
 # Register your models here.
+
+
+class MyOrganizationAdmin(admin.ModelAdmin):
+    model = Organization
+    list_display = ('org_name',)
+
+
+admin.site.register(Organization, MyOrganizationAdmin)
+
+
+class MyDepartmentAdmin(admin.ModelAdmin):
+    model = Department
+    list_display = ('dep_name', 'organization')
+
+
+admin.site.register(Department, MyDepartmentAdmin)
+
+
+class MyEmployeeAdmin(admin.ModelAdmin):
+    model = Employee
+    list_display = ('emp_name', 'department', 'status')
+
+
+admin.site.register(Employee, MyEmployeeAdmin)
--- main/api/models.py
+++ main/api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+
+class Organization(models.Model):
+    org_name = models.CharField(max_length=100)
+
+    def slug(self):
+        return slugify(self.org_name)
+
+    def __str__(self):
+        return self.org_name
+
+
+class Department(models.Model):
+    dep_name = models.CharField(max_length=100)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def slug(self):
+        return slugify(self.dep_name)
+
+    def __str__(self):
+        return self.dep_name
+
+
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    status_choices = (
+        ('1', 'Manager'),
+        ('2', 'Developer'),
+        ('3', 'Boss')
+    )
+    status = models.CharField(max_length=1, choices=status_choices)
+
+    def slug(self):
+        return slugify(self.emp_name)
+
+    def __str__(self):
+        return self.emp_name
