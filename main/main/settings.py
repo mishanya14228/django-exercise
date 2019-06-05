13358340d8c61ca66bccf8ac1db14261de180060
Oysiyl <logart1995@gmail.com>
2019-06-05 14:57:37 +0300

    Revert "Added Models"

--- main/main/settings.py
+++ main/main/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
